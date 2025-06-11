168d3a77335bc126e35e61b89893f2a8ab4445c4
MarinaGloc <marina_gloc@hotmail.com>
2025-06-11 17:30:16 -0300

    Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Marina Gloc
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+    NetUpdateFrequency = 100.f; // Set the network update frequency to 100 Hz
+}

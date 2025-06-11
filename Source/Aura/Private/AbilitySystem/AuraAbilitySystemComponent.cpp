7f6d4307055eb4aee48fef7f5d7f5903acaad0c0
MarinaGloc <marina_gloc@hotmail.com>
2025-06-11 17:38:12 -0300

    Ability system component and Attribute set

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Marina Gloc
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright Marina Gloc
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};

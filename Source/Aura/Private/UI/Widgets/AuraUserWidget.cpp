4ce0cc44d7ef2b63f0c8eb31610767515d5b746f
MarinaGloc <marina_gloc@hotmail.com>
2025-06-12 21:04:33 -0300

    Aura user widget and widget controller

---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Marina Gloc
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+    WidgetController = InWidgetController;
+    WidgetControllerSet();
+}
